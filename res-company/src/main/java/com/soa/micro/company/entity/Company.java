e3223539cf85341a59add1dde86150e158b0aaa1
小狼 <719738494@qq.com>
2018-03-14 01:39:45 +0800

    add:添加并发数据更新的解决方案

--- res-company/src/main/java/com/soa/micro/company/entity/Company.java
+++ res-company/src/main/java/com/soa/micro/company/entity/Company.java
@@ -2,14 +2,40 @@ package com.soa.micro.company.entity;
 
 import java.util.List;
 
-import lombok.AllArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.persistence.Version;
+
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_company")
 public class Company {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 
 	private String name;
 
+	private Integer staffCount = 0;
+
+	@Version
+	private int version;
+
+	@Transient
 	private List<User> users;
+
+	public Company(String name, List<User> users) {
+		this.name = name;
+		this.users = users;
+	}
+
 }
---
+++ res-company/src/main/java/com/soa/micro/company/exeception/ModifidException.java
@@ -0,0 +1,19 @@
+package com.soa.micro.company.exeception;
+
+public class ModifidException extends RuntimeException {
+
+	private static final long serialVersionUID = 928421818937263886L;
+
+	public ModifidException() {
+		super();
+	}
+
+	public ModifidException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ModifidException(String message) {
+		super(message);
+	}
+
+}
